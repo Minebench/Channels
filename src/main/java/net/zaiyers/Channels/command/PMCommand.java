e78c51de0b38765b7397373148f11d809e4fdbd6
Phoenix616 <mail@moep.tv>
2017-02-25 20:00:33 +0100

    Fix pm command

--- src/main/java/net/zaiyers/Channels/command/PMCommand.java
+++ src/main/java/net/zaiyers/Channels/command/PMCommand.java
@@ -37,7 +37,7 @@ public class PMCommand extends AbstractCommand {
 		if (args.length > 0) {
 			//set recipient
 			Chatter recipient = Channels.getInstance().getChatterByName(args[0]);
-			if (recipient == null || recipient.getPlayer() == null || recipient.getPlayer().isConnected()) {
+			if (recipient == null || recipient.getPlayer() == null || !recipient.getPlayer().isConnected()) {
 				//nobody matched
 				Channels.notify(sender, "channels.command.chatter-not-found", ImmutableMap.of("chatter", args[0]));
 			} else if (args.length == 1) {
